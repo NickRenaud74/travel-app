6f8881131f6058202a89f6696421ce7a6698d9a9
Nick Renaud <nick_renaud@hotmail.com>
2020-09-24 10:40:04 -0700

    Configure server.js setup

---
+++ src/server/server.js
@@ -0,0 +1,35 @@
+// Global variables holding project data
+const projectData = {};
+
+// Initializing express
+const express = require('express');
+const app = express();
+
+//Initializing cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+//Configure body-parser as middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('index.html'));
+
+// port and server connection
+const port = '8000';
+
+const server = app.listen(port, () => {
+    console.log(`Server is listening on localhost: ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile('C:/UDAC - FrontEnd/travelApp/src/client/views/index.html');
+})
+
+// Get Route
+app.get('/projectData', (req, res) => {
+    res.send(projectData);
+});
+
+// Post Route

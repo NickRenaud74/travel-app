7a504723196f7addbd5fa5e1253f66b92ece38d2
Nick Renaud <nick_renaud@hotmail.com>
2020-09-24 19:37:41 -0700

    feat: Get request to Geonames API
    set up server requirements with all project dependencies.  Create a
    global variable to store project data that will be returned to the user
    interface.
    set up a get request to the geonames API to fetch data of a location
    that has been passed in from the user.

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,26 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
+// Configure dotenv to access envrionment variables
+const dotenv = require('dotenv');
+dotenv.config();
+
 // Global variables holding project data
 const projectData = {};
 
-// Initializing express
-const express = require('express');
-const app = express();
-
-//Initializing cors for cross origin allowance
-const cors = require('cors');
-app.use(cors());
-
-//Configure body-parser as middleware
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// creating an instance of express
+const app = express()
+    //to use cross origin allowance
+app.use(cors())
+    // to use json
+app.use(bodyParser.json())
+    // to use url encoded values
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 app.use(express.static('dist'));
 
@@ -25,11 +33,27 @@ const server = app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.sendFile('dist/index.html');
-})
+});
 
 // Get Route
 app.get('/projectData', (req, res) => {
     res.send(projectData);
 });
 
-// Post Route
+//GeoNames API data
+const geoUrl = 'http://api.geonames.org/searchJSON?';
+const username = process.env.geoUsername;
+
+//request to Geonames API with placename
+const getCoords = async(req, res) => {
+    const input = req.body.destination;
+    const response = await fetch(`${geoUrl}q=${input}&maxRows=10&username=${username}`);
+    try {
+        const geoData = await response.json();
+        console.log(geoData);
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+app.post('/geonames', getCoords);

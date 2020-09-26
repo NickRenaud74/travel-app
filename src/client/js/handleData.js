0eb5fe7da935f05b781bc0e8037b4d949e3833c0
Nick Renaud <nick_renaud@hotmail.com>
2020-09-26 16:07:48 -0700

    feat: Add CalendarCountdown function
    - Add function to calculate the time in days from current date to trip
    departure date.

--- src/client/js/handleData.js
+++ src/client/js/handleData.js
@@ -5,6 +5,21 @@ import { postGeo } from './geonames'
 const getData = async(event) => {
     event.preventDefault();
     let place = document.getElementById('destination').value;
+    let trip = document.getElementById('date').value;
+    console.log(trip);
+
+    //Create a countdown from current date to user inputted date
+    const calendarCountdown = () => {
+        let tripDate = new Date(document.getElementById('date').value);
+        console.log(tripDate.toUTCString());
+        const date = new Date();
+        const difference = tripDate - date;
+        const daysToTrip = (Math.ceil(difference / (1000 * 60 * 60 * 24)));
+        console.log(difference, daysToTrip);
+        return { daysToTrip, tripDate };
+    };
+    calendarCountdown();
+
 
     postGeo('/geonames', { destination: place })
         .then(async(data) => {

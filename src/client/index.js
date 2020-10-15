a514e1b3898174a6bb1b907969302894ef6ebf09
Nick Renaud <nick_renaud@hotmail.com>
2020-10-15 10:07:15 -0700

    feat: Import background from image folder

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,8 @@
 import { getData } from './js/handleData';
 import { updateUi } from './js/updateUi';
+import './styles/main.scss';
+import './styles/form.scss';
+import background from '../../images/background.jpg';
 
 export {
     getData,

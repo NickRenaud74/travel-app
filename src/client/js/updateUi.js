0d17fb7e5cb327cbb5b735e5c7690df12ed2780f
Nick Renaud <nick_renaud@hotmail.com>
2020-10-17 12:14:05 -0700

    refactor: refactor to allow for multiple trips
    -Refactor the updateUi function to allow the user to add multiple trips.
    -Create a trip card for each trip added by the user with data passed in
    from the form
    -Add event listeners for the image modal and delete trip button so user
    can view full size images, and delete a trip.
    - Move all helper functions to a seperate file for easier readability,
    accessing through the client library.

--- src/client/js/updateUi.js
+++ src/client/js/updateUi.js
@@ -1,112 +1,56 @@
-let slideIndex = 0;
-
-const slideShow = () => {
-    //console.log(slides.length);
-    const slides = document.querySelectorAll('.slides');
-    for (let slide of slides) {
-        slide.style.display = 'none';
-    };
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    };
-    slides[slideIndex - 1].style.display = 'block';
-    setTimeout(slideShow, 3000);
-};
-
+//Dynamic UI function
 const updateUi = async() => {
     const request = await fetch('/projectData');
     try {
         const projectData = await request.json();
 
-        //create div to hold output
-        let trip = document.getElementById('trip');
+        //create an array to hold all trips added by user
+        let allTrips = Object.values(projectData);
 
-        //div to hold picture details
-        const display = document.createElement('div');
-        display.className = 'display';
+        //get container element
+        const container = document.querySelector('.container');
 
-        //setting up picture variables for slideshow
-        const pics = projectData.pictures;
-        const fragment = document.createDocumentFragment();
+        /*remove all trip cards from DOM.
+        Each time a new trip is added the ForEach loop below will update all saved trips.*/
+        const getAllTrips = document.querySelectorAll('.trip');
+        getAllTrips.forEach(trip => {
+            trip.remove();
+        });
 
-        //Create image element for each picture in list
-        for (let pic of pics) {
-            const slide = document.createElement('img');
-            slide.src = pic;
-            slide.className = 'slides';
-            fragment.appendChild(slide);
-        };
-        display.appendChild(fragment);
-        trip.appendChild(display);
-
-        // Automatic slideshow function
-        slideShow();
-
-        //Create a div to hold trip information
-        const tripDetails = document.createElement('div');
-        tripDetails.className = 'details';
-
-        //Description of trip
-        const description = document.createElement('div');
-        description.className = 'description';
-        description.innerHTML =
-            `<p><strong>Your ${projectData.length} day trip to ${projectData.city}, ${projectData.country} leaving on ${projectData.tripDate}.</strong></p>`;
-        tripDetails.appendChild(description);
-
-        //Countdown to trip
-        const countdown = document.createElement('div');
-        countdown.className = 'countdown';
-        countdown.innerHTML = `<p><strong>${projectData.countdown} days away!</strong></p>`;
-        tripDetails.appendChild(countdown);
-
-        //update destination information
-        const info = document.createElement('div');
-        info.className = 'info';
-        info.innerHTML =
-            `<h3><strong>Things to know before you go</strong></h3>
-            <ul>
-            <li><strong>Capital City:</strong> ${projectData.capital}</li>
-            <li><strong>Population of ${projectData.country}:</strong> ${projectData.population}</li>
-            <li><strong>Language spoken:</strong> ${projectData.language}</li>
-            <li><strong>Currency:</strong> ${projectData.currency}</li>
-            </ul>`;
-        tripDetails.appendChild(info);
-
-        //Display forecast icon, description and temperatures for today
-        const forecast = document.createElement('div');
-        forecast.className = 'forecast';
-        if (projectData.countdown <= 7) {
-            forecast.innerHTML =
-                `<div class="icon"><img src="https://www.weatherbit.io/static/img/icons/${projectData.forecast[0].weather.icon}.png" alt="weather icon"></div>
-                <div class="conditions"><strong>Current weather conditions:</strong> ${projectData.forecast[0].weather.description}.
-                <div class="temp"><strong>High: </strong>${projectData.forecast[0].max}&degC <strong>Low: </strong>${projectData.forecast[0].min}&degC</div>`
-        };
-
-        //Display 16 day forecast if trip is longer than 7 days
-        if (projectData.countdown > 7) {
-            const longForecast = document.createElement('ul');
-            longForecast.className = 'long-forecast';
-            for (let day of projectData.forecast) {
-                const dailyForecast = document.createElement('li');
-                dailyForecast.innerHTML =
-                    `<div class="date">${day.date}</div>
-                    <div class="icon"><img src="https://www.weatherbit.io/static/img/icons/${day.weather.icon}.png" alt="weather icon"></div>
-                <div class="conditions">${day.weather.description}.</div>
-                <div class="temp"><strong>High: </strong>${day.max}&degC <strong>Low: </strong>${day.min}&degC</div>`
-                longForecast.appendChild(dailyForecast);
-            };
-            forecast.appendChild(longForecast);
-        }
-        tripDetails.appendChild(forecast);
-
-        trip.appendChild(tripDetails);
-
+        //iterate through each destination in all trips array and create an html trip card
+        allTrips.forEach(dest => {
+            //create div to hold each trip
+            const trip = document.createElement('div');
+            trip.className = 'trip';
+            trip.id = `${dest.tripId}`;
+            trip.innerHTML = `
+            <div class="display">${Client.getImages(dest.pictures)}
+                <div class="info"><h3><strong>Things to know before you go</strong></h3>
+                <ul>
+                <li><strong>Capital City:</strong> ${dest.capital}</li>
+                <li><strong>Population of ${dest.country}:</strong> ${dest.population}</li>
+                <li><strong>Language spoken:</strong> ${dest.language}</li>
+                <li><strong>Currency:</strong> ${dest.currency}</li>
+                </ul></div>
+            </div>
+            <div class="details">
+                <div class="description"><p><strong>Your ${dest.length} day trip to ${dest.city}, ${dest.country} leaving on ${dest.tripDate}.</strong></p></div>
+                <div class="countdown"><p><strong>${dest.countdown} days away!</strong></p></div>
+                <div class="btn"><button class="delete-trip" id=${dest.tripId}>Delete Trip</button></div>
+                <div class="forecast">${Client.getForecast(dest.countdown, dest.forecast)}</div>
+            </div>`
+            container.appendChild(trip);
+        });
+        document.querySelectorAll('.slides').forEach(slide => {
+            slide.addEventListener('click', Client.displayModal)
+        });
+        document.querySelectorAll('.delete-trip').forEach(button => {
+            button.addEventListener('click', Client.deleteTrip)
+        });
     } catch (error) {
         console.log('error: ', error);
     }
 };
-
 
 export {
     updateUi

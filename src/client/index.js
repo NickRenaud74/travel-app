d3450eacfb693b0f2ada0696f643586c2c7dc047
Nick Renaud <nick_renaud@hotmail.com>
2020-10-17 12:06:25 -0700

    feat: export functions to Client library
    -Exporting functions to Client library so they can be used throughout
    all files in the project.

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,18 @@
 import { getData } from './js/handleData';
 import { updateUi } from './js/updateUi';
+import { postData } from './js/postData';
+import { getForecast, displayModal, getImages, deleteTrip } from './js/helpers';
 import './styles/main.scss';
 import './styles/form.scss';
 import background from '../../images/background.jpg';
 
+//export functions to Client library
 export {
     getData,
-    updateUi
+    updateUi,
+    postData,
+    getForecast,
+    displayModal,
+    getImages,
+    deleteTrip
 }

442d4c546ff9c65d13d8c1c0a28843456b1e7145
Nick Renaud <nick_renaud@hotmail.com>
2020-10-18 14:25:05 -0700

    feat: add previous trips section
    -Add previous trip section, if countdown less than 0 append trip-card to
    the previous trips div

--- src/client/js/updateUi.js
+++ src/client/js/updateUi.js
@@ -1,22 +1,13 @@
 //Dynamic UI function
-const updateUi = async() => {
-    const request = await fetch('/projectData');
+const updateUi = async(getTrips) => {
     try {
-        const projectData = await request.json();
-
-        /* Check if local storage exists
-        let tripsArray = localStorage.getItem("trips") ? JSON.parse(localStorage.getItem("trips")) : [];
-
-        const tripData = () => {
-            let getStorage = localStorage.getItem('trips');
-            return JSON.parse(getStorage.trip);
-        }*/
-
-        //create an array to hold all trips added by user
-        let allTrips = Object.values(projectData);
-
-        //get container element
+        //get main container element to hold upcoming trips
         const container = document.querySelector('.container');
+        container.innerHTML = '<div class="my-trips"><h2>Upcoming Trips</h2></div>';
+
+        //get previous trips div
+        const expired = document.querySelector('.previous-trips');
+        expired.innerHTML = `<div class="my-trips"><h2>Previous Trips</h2></div>`;
 
         /*remove all trip cards from DOM.
         Each time a new trip is added the ForEach loop below will update all saved trips.*/
@@ -26,7 +17,10 @@ const updateUi = async() => {
         });
 
         //iterate through each destination in all trips array and create an html trip card
-        allTrips.forEach(dest => {
+        getTrips.forEach(dest => {
+            //if countdown is less than a day away change days string to day
+            let days = dest.countdown > 1 ? 'days' : 'day';
+
             //create div to hold each trip
             const trip = document.createElement('div');
             trip.className = 'trip';
@@ -42,18 +36,23 @@ const updateUi = async() => {
                 </ul></div>
             </div>
             <div class="details">
-                <div class="description"><p><strong>Your ${dest.length} day trip to ${dest.city}, ${dest.country} leaving on ${dest.tripDate}.</strong></p></div>
-                <div class="countdown"><p><strong>${dest.countdown} days away!</strong></p></div>
+                <div class="description">
+                    <p><strong>Your ${dest.length} day trip to ${dest.city}, ${dest.country} on ${dest.tripDate}.</strong></p>
+                    <div class="countdown"><p><strong>${dest.countdown} ${days} away!</strong></p></div>
+                </div>
+                
                 <div class="btn"><button class="delete-trip" id=${dest.tripId}>Delete Trip</button></div>
                 <div class="forecast">${Client.getForecast(dest.countdown, dest.forecast)}</div>
             </div>`
-            container.appendChild(trip);
 
-            //tripsArray.push(dest);
-            //localStorage.setItem('trips', JSON.stringify(tripsArray));
-
+            //Check if trip is has expired or not
+            if (dest.countdown > 0) {
+                container.appendChild(trip);
+            } else {
+                expired.appendChild(trip);
+                document.querySelector('.countdown').remove();
+            };
         });
-        console.log(tripData());
         document.querySelectorAll('.slides').forEach(slide => {
             slide.addEventListener('click', Client.displayModal)
         });

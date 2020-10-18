a38c2cd9f3544e27499f95c398d199b592670cc9
Nick Renaud <nick_renaud@hotmail.com>
2020-10-18 14:16:56 -0700

    feat: add event listener on page load
    -Once DOM has fully loaded datepicker attributes will be set to get
    Todays Date and ensure the user cannot choose a past date.
    
    -Incorporate Local storage into project, on page load will check if
    there is any local storage, and update UI accordingly with previous
    data.

--- src/client/index.js
+++ src/client/index.js
@@ -2,9 +2,34 @@ import { getData } from './js/handleData';
 import { updateUi } from './js/updateUi';
 import { postData } from './js/postData';
 import { getForecast, displayModal, getImages, deleteTrip } from './js/helpers';
+import { formValidation, dateValidation } from './js/formValidation';
 import './styles/main.scss';
 import './styles/form.scss';
-import background from '../../images/background.jpg';
+import background from '../../images/paradise.jpg';
+
+
+window.addEventListener('DOMContentLoaded', (e) => {
+    //set datepicker min attributes to be todays date so user can only enter future trip dates
+    const datepicker = document.querySelectorAll('.date');
+    for (let d of datepicker) {
+        d.setAttribute('min', dateValidation());
+    };
+
+    let storage = Object.values(localStorage);
+    if (storage.length > 0) {
+        //remove webpack-dev-server loglevel INFO
+        const getIndex = storage.indexOf('INFO');
+        storage.splice(getIndex, 1);
+        //create a new list and parse each item in local storage
+        let storageData = [];
+
+        storage.forEach(item => {
+            storageData.push(JSON.parse(item));
+        });
+        //update UI with localstorage info
+        Client.updateUi(storageData);
+    }
+});
 
 //export functions to Client library
 export {
@@ -14,5 +39,7 @@ export {
     getForecast,
     displayModal,
     getImages,
-    deleteTrip
+    deleteTrip,
+    formValidation,
+    dateValidation
 }

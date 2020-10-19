e4c68330ff3bff1a92dc89d1e45c583bffd0cfdd
Nick Renaud <nick_renaud@hotmail.com>
2020-10-18 19:00:34 -0700

    chore: add service workers to dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: ['@babel/polyfill', './src/client/index.js'],
@@ -38,5 +39,6 @@ module.exports = {
             filename: './index.html'
         }),
         new MiniCssExtractPlugin({ filename: '[name].css' }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
